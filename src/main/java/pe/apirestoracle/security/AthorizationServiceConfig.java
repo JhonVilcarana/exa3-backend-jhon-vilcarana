216a0a9662836b80d6e3c47f0af151178ba79551
JhonVilcarana <jhonvilcarana@upeu.edu.pe>
2020-11-26 15:18:13 -0500

    final

--- src/main/java/pe/apirestoracle/security/AthorizationServiceConfig.java
+++ src/main/java/pe/apirestoracle/security/AthorizationServiceConfig.java
@@ -33,7 +33,7 @@ public class AthorizationServiceConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		// TODO Auto-generated method stub
-		clients.inMemory().withClient("etesech")
+		clients.inMemory().withClient("aea")
 		.secret(passwordEncoder.encode("123"))
 		.scopes("red", "write")
 		.authorizedGrantTypes("password","refresh_token")
